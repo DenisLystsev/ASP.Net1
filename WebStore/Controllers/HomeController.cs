84a33d601f1a37a1dc1f0addf3e25a577046a7a1
Denis Lystsev (job) <dlyst@mail.ru>
2018-11-06 17:17:15 +0300

    Исправлены ошибки

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebStoreTest.Models;
+using WebStore.Models;
 
-namespace WebStoreTest.Controllers
+namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
--- WebStore/Models/EmployeeView.cs
+++ WebStore/Models/EmployeeView.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebStoreTest.Models
+namespace WebStore.Models
 {
     public class EmployeeView
     {
