5897c4b607a6a09ffd5d16226dfc4853aba4cde7
Denis Lystsev (job) <dlyst@mail.ru>
2018-11-06 17:13:24 +0300

    Заполнены свойства модели EmployeeView. В HomeController добавлены данные для отображения, исправлен метод Index

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStoreTest.Models;
 
-namespace WebStore.Controllers
+namespace WebStoreTest.Controllers
 {
     public class HomeController : Controller
     {
+        public EmployeeView Person;
+
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                LastName = "Иванов",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                Patronymic = "Степанович",
+                LastName = "Оченьстариков",
+                Age = 125
+            }
+        };
+
         public IActionResult Index()
         {
-            return View();
+            return View(_employees);
         }
     }
 }
--- WebStore/Models/EmployeeView.cs
+++ WebStore/Models/EmployeeView.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebStore.Models
+namespace WebStoreTest.Models
 {
     public class EmployeeView
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
