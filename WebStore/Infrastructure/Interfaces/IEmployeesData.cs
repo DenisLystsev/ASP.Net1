3af10c414e74845525db594f61db1935a09e78c5
Denis Lystsev (home) <dlyst@mail.ru>
2018-11-14 22:41:28 +0300

    Создан интерфейс для работы с сотрудниками

---
+++ WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    //Интерфейс для работы с сотрудниками
+
+    interface IEmployeesData
+    {
+        //Получение списка сотрудников
+        IEnumerable<EmployeeView> GetAll();
+
+        //Получение сотрудника по Id
+        EmployeeView GetById(int id);
+
+        //Сохранить изменения
+        void Commit();
+
+        //Добавить нового сотрудника
+        void AddNew(EmployeeView model);
+
+        //Удалить сотрудника
+        void Delete(int id);
+    }
+}
