33b81b505e1f78ff4f3db51453b0a61164903126
Denis Lystsev (home) <dlyst@mail.ru>
2018-11-14 22:53:00 +0300

    Добавлен класс InMemoryEmployeesData

---
+++ WebStore/Infrastructure/Implementations/InMemoryEmployeesData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    //Реализация интерфейса IEmployeesData, хранит информацию в памяти
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+
+    }
+}
