f1d592bf4030fbaff224abceff0c2b9096805927
Denis Lystsev (job) <dlyst@mail.ru>
2018-11-19 10:19:42 +0300

    Изменил EmployeeView для добавления названий полей на форме редактирования сотрудника на русском языке

--- WebStore/Models/EmployeeView.cs
+++ WebStore/Models/EmployeeView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,20 @@ namespace WebStore.Models
     public class EmployeeView
     {
         public int Id { get; set; }
+
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+
+        [Display(Name = "Должность")]
         public string Position { get; set; }
     }
 }
