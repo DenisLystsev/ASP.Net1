71598d172fb2bab7e65aa11f93ad58f6ce8db3d3
Denis Lystsev (home) <dlyst@mail.ru>
2018-11-21 16:28:04 +0300

    Создал сущность Товар

---
+++ WebStore.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Models.Base;
+
+namespace WebStore.Domain.Models
+{
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        // Секция, к которой принадлежит товар
+        public int SectionId { get; set; }
+        // Бренд товара
+        public int? BrandId { get; set; }
+        // Ссылка на картинку
+        public string ImageUrl { get; set; }
+        // Цена
+        public decimal Price { get; set; }
+    }
+}
