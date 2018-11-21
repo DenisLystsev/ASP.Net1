323441aac852c8fcd5d924bf25af89180c9528e4
Denis Lystsev (home) <dlyst@mail.ru>
2018-11-21 16:43:29 +0300

    Создал ProductFilter для фильтрации товаров

---
+++ WebStore.Domain/Filters/ProductFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Filters
+{
+    public class ProductFilter
+    {
+        // Секция, к которой принадлежит товар
+        public int? SectionId { get; set; }
+        // Бренд товара
+        public int? BrandId { get; set; }
+    }
+}
